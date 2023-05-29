c488eb18a053322088c42f995f793aee7f702e70
Loghin Dan <loghindan494@gmail.com>
2023-05-29 18:37:34 +0300

    almost all js

--- js/images.js
+++ js/images.js
@@ -1,9 +1,44 @@
-let leftImages=[
-    {
-        id:1,
-        name: "rock",
-        image: "/img/rock_left.png"
-    }
-]
+let randomNumber;
 
-console.log(leftImages[0])
+let imageLeft = document.getElementById("img_left")
+
+function changeImageLeft() {
+if (randomNumber === 1) {
+    imageLeft.src = "/img/rock_left.png"
+};
+
+if (randomNumber === 2) {
+    imageLeft.src = "/img/paper_left.png"
+};
+
+if (randomNumber === 3) {
+    imageLeft.src = "/img/scissors_left.png"
+};
+};
+
+let rockBt = document.getElementById("rock");
+let paperBt = document.getElementById("paper");
+let scissorsBt = document.getElementById("scissors");
+
+let imageRight = document.getElementById("img_right");
+
+function rockFc() {
+    imageRight.src = "/img/rock_right.png";
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+    changeImageLeft();
+    setTimeout(3000)
+};
+
+function paperFc() {
+    imageRight.src = "/img/paper_right.png";
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+    changeImageLeft();
+    setTimeout(3000)
+};
+
+function scissorsFc() {
+    imageRight.src = "/img/scissors_right.png";
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+    changeImageLeft();
+    setTimeout(3000)
+};
