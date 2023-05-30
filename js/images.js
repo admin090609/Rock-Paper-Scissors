c5cfc5fa663478aa8a120ca6158e8144cb5a43b8
Loghin Dan <loghindan494@gmail.com>
2023-05-30 14:52:50 +0300

    Final

--- js/images.js
+++ js/images.js
@@ -3,17 +3,15 @@ let randomNumber;
 let imageLeft = document.getElementById("img_left")
 
 function changeImageLeft() {
-if (randomNumber === 1) {
-    imageLeft.src = "/img/rock_left.png"
-};
-
-if (randomNumber === 2) {
-    imageLeft.src = "/img/paper_left.png"
-};
-
-if (randomNumber === 3) {
-    imageLeft.src = "/img/scissors_left.png"
-};
+    if (randomNumber === 1) {
+        imageLeft.src = "/img/rock_left.png"
+    }
+    else if (randomNumber === 2) {
+        imageLeft.src = "/img/paper_left.png"
+    }
+    else if (randomNumber === 3) {
+        imageLeft.src = "/img/scissors_left.png"
+    };
 };
 
 let rockBt = document.getElementById("rock");
@@ -26,19 +24,46 @@ function rockFc() {
     imageRight.src = "/img/rock_right.png";
     randomNumber = Math.floor(Math.random() * 3) + 1;
     changeImageLeft();
-    setTimeout(3000)
+    checkResult()
 };
 
 function paperFc() {
     imageRight.src = "/img/paper_right.png";
     randomNumber = Math.floor(Math.random() * 3) + 1;
     changeImageLeft();
-    setTimeout(3000)
+    checkResult()
 };
 
 function scissorsFc() {
     imageRight.src = "/img/scissors_right.png";
     randomNumber = Math.floor(Math.random() * 3) + 1;
     changeImageLeft();
-    setTimeout(3000)
+    checkResult()
 };
+
+let resultMessage = document.getElementById("result_message");
+
+function checkResult() {
+    if (imageRight.src.includes("scissors") && imageLeft.src.includes("rock")) {
+        resultMessage.textContent = "You lost!";
+    } else if (imageRight.src.includes("scissors") && imageLeft.src.includes("paper")) {
+        resultMessage.textContent = "You won!";
+    } else if (imageRight.src.includes("paper") && imageLeft.src.includes("scissors")) {
+        resultMessage.textContent = "You lost!";
+    } else if (imageRight.src.includes("paper") && imageLeft.src.includes("rock")) {
+        resultMessage.textContent = "You won!";
+    } else if (imageRight.src.includes("rock") && imageLeft.src.includes("paper")) {
+        resultMessage.textContent = "You lost!";
+    } else if (imageRight.src.includes("rock") && imageLeft.src.includes("scissors")) {
+        resultMessage.textContent = "You won!";
+    } else if (imageRight.src.includes("paper") && imageLeft.src.includes("paper")) {
+        resultMessage.textContent = "Draw!";
+    } else if (imageRight.src.includes("rock") && imageLeft.src.includes("rock")) {
+        resultMessage.textContent = "Draw!";
+    } else if (imageRight.src.includes("scissors") && imageLeft.src.includes("scissors"))
+        resultMessage.textContent = "Draw!"
+    else {
+        resultMessage.textContent = "";
+    }
+
+}
